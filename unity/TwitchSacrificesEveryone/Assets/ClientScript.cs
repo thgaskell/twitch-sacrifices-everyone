a901ed7cbe4f07f2ac248f671a92b78fad483187
AllNamesTaken <skylight.1992@hotmail.com>
2016-01-29 22:35:00 -1000

    Started WWW get script

---
+++ unity/TwitchSacrificesEveryone/Assets/ClientScript.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ClientScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public WWW GET(string url)
+	{
+		WWW www = new WWW (url);
+		StartCoroutine(
+	}
+}
