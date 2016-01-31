adb76e6a39d79bb50e67f5760526ec75939b7b15
JesseTho <JesseTho@hawaii.edu>
2016-01-31 12:28:48 -1000

    Magic Circle Prefab
    It Spins!

---
+++ unity/TwitchSacrificesEveryone/Assets/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour
+{
+	public float speed = 10f;
+	public float x = 0f;
+	public float y = 0f;
+	public float z = 0f; 
+
+	void Update ()
+	{
+		transform.Rotate(new Vector3(x,y,z), speed * Time.deltaTime);
+	}
+}
